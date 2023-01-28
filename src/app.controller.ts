a3cd0e20e682c662cac42c07fb4df4b866a9f82a
cherrycollapse <cherrycollapse@gmail.com>
2023-01-28 11:24:55 +0200

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, StreamableFile, Res, Header } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import type { Response } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get()
+  @Header('Content-Type', 'application/json')
+  @Header('Content-Disposition', 'attachment; filename="res.json"')
+  getStaticFile(): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'res.json'));
+    return new StreamableFile(file);
+  }  
+
+
+}
